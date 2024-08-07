d3b2677593dbfbe5b004ca10100e27015012b1f1
amolpethkar <65155248+amolpethkar@users.noreply.github.com>
2024-08-07 17:19:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,7 @@ input=st.sidebar.text_input("Input Prompt: ",key="input")
 uploaded_file = st.sidebar.file_uploader("Choose an image of the Invoice", type=["jpg", "jpeg", "png"])
 image=""   
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.sidebar.image(image, caption="Uploaded Image.", use_column_width=True)
+
 
 submit=st.sidebar.button("Ask Me")
 
@@ -64,11 +62,16 @@ You are an expert in understanding the invoices. We will upload a image as invoi
 will have to answer any question based on the uploaded invoice image.
 """
 
-if submit:   
-        image_data = input_image_setup(uploaded_file)
-        response=get_gemini_response(input_prompt,image_data,input)
-        st.subheader("The Response is")
-        st.write(response)
+if submit:
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.sidebar.image(image, caption="Uploaded Image.", use_column_width=True)
+            image_data = input_image_setup(uploaded_file)
+            response=get_gemini_response(input_prompt,image_data,input)
+            st.subheader("The Response is")
+            st.write(response)
+        else
+             st.write("No image uploaded.")
 
    
 
